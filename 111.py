17aa7a03e079768f7100bc0d10e6260603f161cd
ABC <dl2349@126.com>
2019-05-05 20:39:38 +0800

    scz

---
+++ 111.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/4 21:50
+# @Author  : SCZ
+# @File    : 111.py
---
+++ python基础/helloword.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/4 21:48
+# @Author  : SCZ
+# @File    : helloword.py
+# ！/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+print("hello word!")
+print(8*'abc')
+name = input('请输入：')
+print('hello,',name)
+name = input('please enter your name: ')
+print('hello,', name)
+print('I\'m learning\nPython.')
